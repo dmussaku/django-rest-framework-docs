0f94c7e308aca54cf72cb805f8da55c3781936a4
daniyar <daniyar@aviata.me>
2017-01-23 19:26:05 +0600

    Add template tag

--- rest_framework_docs/templatetags/drfdocs_filters.py
+++ rest_framework_docs/templatetags/drfdocs_filters.py
@@ -10,3 +10,11 @@ register = template.Library()
 @stringfilter
 def markdown(value):
     return markup_description(value)
+
+
+@register.filter(name='first_arg_split_by_slash')
+@stringfilter
+def split_by_slash(value, name_parent):
+    value = value.split(name_parent)[1]
+    result = value.split('/')[1]
+    return result if result else name_parent
